27a73db49a9d4a0ac0b91a71ea219996703af320
Lee Driscoll <lsdriscoll@gmail.com>
2017-01-13 16:35:58 +0000

    Added UID/GID fix

--- localdb-run.sh
+++ localdb-run.sh
@@ -4,11 +4,11 @@ set -e # fail on any error
 echo '* Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
 TARGET_UID=$(stat -c "%u" /var/lib/mysql)
 echo '-- Setting mysql user to use uid '$TARGET_UID
-usermod -o -u $TARGET_UID mysql || true
+usermod -u $TARGET_UID mysql || true
 TARGET_GID=$(stat -c "%g" /var/lib/mysql)
 echo '-- Setting mysql group to use gid '$TARGET_GID
-groupmod -o -g $TARGET_GID mysql || true
+groupmod -g $TARGET_GID mysql || true
 echo
-echo '* Starting MySQL'
+#echo '* Starting MySQL'
 chown -R mysql:root /var/run/mysqld/
-/entrypoint.sh mysqld --user=mysql --console
+#/entrypoint.sh mysqld --user=mysql --console
