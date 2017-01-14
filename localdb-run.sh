cb3e23558891772107a43a5515e2a6b5d5ef49a8
Lee Driscoll <lsdriscoll@gmail.com>
2017-01-14 15:59:49 +0000

    hard-coded uid + gid

--- localdb-run.sh
+++ localdb-run.sh
@@ -2,10 +2,12 @@
 set -e # fail on any error
 
 echo '* Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
-TARGET_UID=$(id -u mysql)
+#TARGET_UID=$(id -u mysql)
+TARGET_UID=60
 echo '-- Setting mysql user to use uid '$TARGET_UID
 usermod -u $TARGET_UID mysql || true
-TARGET_GID=$(id -g mysql)
+#TARGET_GID=$(id -g mysql)
+TARGET_GID=493
 echo '-- Setting mysql group to use gid '$TARGET_GID
 groupmod -g $TARGET_GID mysql || true
 echo
